b2d295100fe845c92f43e4aa0a2edc3ccbda6b94
yangl1992 <448322573@qq.com>
2017-10-19 23:05:18 +0800

    11111

--- src/components/common/leftNav.vue
+++ src/components/common/leftNav.vue
@@ -1,5 +1,5 @@
 <<template>
-  <div>
+  <div class="left-nav">
     <ul>
       <li>
         <i class="icon iconfont icon-goumai"></i>
@@ -28,6 +28,26 @@ export default {
 </script>
 
 <style>
-
+.left-nav{
+  color: #fff;
+  font-size: 10px;
+  height: 100%;
+  background-color: #1d8ce0;
+  float: left;
+  width: 5%;
+}
+.iconfont{
+  font-size: 24px;
+}
+.left-nav ul{
+  padding: 0;
+  margin: 0;
+}
+.left-nav li{
+  list-style: none;
+  text-align: center;
+  border-bottom: 1px solid #20a0ff;
+  padding: 10px;
+}
 </style>
 
